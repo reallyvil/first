343d9d506d3960b3e8580c2d9d4b0da6940bef79
Viliame Tuitoga <Viliame.Tuitoga@s.elim.school.nz>
2019-03-14 12:44:42 +1300

    First Time
    forgot to add a version control before I started, luckily I wasn't in the middle of coding.

---
+++ Assets/player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Transform playerSpawnPoints; // The original spawn point
+    public bool respawn = false;
+
+    private Transform[] spawnPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //remember to comment m8
+        spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform>();
+        print(spawnPoints.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
