e2dbe8baa698f83818601a6d09d8ad6e46782cba
Viliame Tuitoga <Viliame.Tuitoga@s.elim.school.nz>
2019-04-29 10:43:58 +1200

    working on main menu
    added a scene that has a UI menu and also added script for it

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : UnityEngine.MonoBehaviour
+{
+    // Void for changing scenes
+    void Load()
+    {
+        // if user clicks the play button
+        if (UnityEngine.Input.GetMouseButtonDown(0))
+            SceneManager.LoadScene("Level");
+    }
+}
