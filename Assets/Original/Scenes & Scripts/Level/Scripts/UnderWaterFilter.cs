bf103528e03f5d107388682f61ec8ec6a4407ead
reallyvili <reallyvili@gmail.com>
2019-06-18 14:07:35 +1200

    removed some unused stuff, organized some folders and added an underwater script filter

---
+++ Assets/Original/Scenes & Scripts/Level/Scripts/UnderWaterFilter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderWaterFilter : MonoBehaviour
+{
+    public float WaterLevel;
+    private boolean inTheWater;
+    private Color outOfTheWater;
+    private Color insideWater;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        outOfTheWater = new Color (0.5f, 0.5f, 0.5f);
+        insideWater = new Color (0.22f, 0.65f, 0.77f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((transform.position.y < WaterLevel) != inTheWater)
+        {
+            inTheWater = transform.position.y < WaterLevel;
+            if (inTheWater) waterFilter();
+            if (!inTheWater) normal();
+        }
+    }
+    void waterFilter()
+    {
+        RenderSettings.fogColor = insideWater;
+        RenderSettings.fogDensity = 0.03f;
+    }
+    void normal()
+    {
+        RenderSettings.fogColor = outOfTheWater;
+        RenderSettings.fogDensity = 0.03f;
+    }
+}
