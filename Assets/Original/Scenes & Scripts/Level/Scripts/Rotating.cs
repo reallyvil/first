d2b4f7b170101dc147fd916fa92bc91f03ce3be0
reallyvili <reallyvili@gmail.com>
2019-07-01 10:09:37 +1200

    nothing too significant

---
+++ Assets/Original/Scenes & Scripts/Level/Scripts/Rotating.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    float speed = 50.0f;
+    // void for rotating the collectable items
+    void Update()
+    {
+        //simply rotates the object by X,Y,Z
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/waterfloating.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterfloating : MonoBehaviour
+{
+    //properties
+    public float airDrag = 1;
+    public float waterDrag = 10;
+    public Transform[] FloatPoints;
+    protected Rigidbody Rigidbody;
+    protected waves Waves;
+    protected float waterLine;
+    protected Vector3[] waterLinePoints;
+    protected Vector3 centerOffset;
+    public Vector3 Center { get {return Transform.position + centerOffset;}}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       Waves = findObjectOfType<water>();
+       Rigidbody = getComponent<Rigidbody>();
+       Rigidbody.useGravity = false; // makes the rigibody float 
+
+       waterLinePoints = new Vector3[FloatPoints.Length];
+       for (int a = 0; a < FloatPoints.Length; a++)
+       waterLinePoints[a] = FloatPoints[a].position;
+       centerOffset = PhysicsSupport.getCenter(waterLinePoints) - Transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
