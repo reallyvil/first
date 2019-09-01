897441bf8158c9c357a908001c50ac6ac3105526
reallyvili <reallyvili@gmail.com>
2019-09-02 10:08:38 +1200

    Added a quick save and reload scene script as well as buttons for these two actions

---
+++ Assets/Original/Scenes & Scripts/Level/Scripts/Quick save/PreviousSave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviousSave : MonoBehaviour
+{
+
+    /* This void is where it records the players position
+    when they saved */
+    void Update()
+    {
+        transform.position += new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("vertical"));
+    }
+    public void LoadTheGame()
+    {
+        //reloads the level but in the position the player last was
+        SaveGame.Load();
+        transform.position = SaveGame.Instance.PlayerPosition;
+    }
+    public void Save()
+    {
+        // saves the game and position the player is
+        SaveGame.Instance.PlayerPosition = transform.position;
+        SaveGame.Save();
+    }
+}
